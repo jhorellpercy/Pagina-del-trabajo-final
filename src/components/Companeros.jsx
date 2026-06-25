fa530f8fb8ba9295c0bb0cc97175bff08e85a701
Jhorell Condori <jhorell.condori@ucsp.edu.pe>
2026-06-24 21:36:06 -0500

    Actualicé la página

--- src/components/Companeros.jsx
+++ src/components/Companeros.jsx
@@ -1,7 +1,7 @@
 const companeros = [
   { nombre: 'Fernando Marco Barra Pérez', url: 'https://fernandoucsp.github.io/proyecto2/' },
   { nombre: 'Enzo Stephen Casapia Vilca', url: 'https://enzovi1009-a11y.github.io/' },
-  { nombre: 'Vanderloth Pacco Huayhua', url: 'https://vanderlothpacco78-code.github.io/Mi-web/' },
+  { nombre: 'Vanderloth Sandro Pacco Huayhua', url: 'https://vanderlothpacco78-code.github.io/Mi-web/' },
 ];
 
 function Companeros() {
